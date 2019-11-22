f965aa1bfd8aece84075d5e8156cceb8bfc74ae1
Ryo Okamoto <ryo@aquahill.net>
2019-11-22 17:51:50 +0900

    added extra maintenance page.

--- build.rb
+++ build.rb
@@ -3,10 +3,20 @@
 require 'yaml'
 require 'erb'
 
-# pages
+# http error pages
 YAML.load_file('./http_status_code.yml').each do |group|
   group['list'].each do |data|
     file = "#{data['code']}.html"
+
+    erb = ERB.new(File.read('./template.html'))
+    File.write file, erb.result(binding).gsub(/\n+/o, "\n")
+  end
+end
+
+# extra pages
+YAML.load_file('./extra.yml').each do |group|
+  group['list'].each do |data|
+    file = "#{data['file']}.html"
 
     erb = ERB.new(File.read('./template.html'))
     File.write file, erb.result(binding).gsub(/\n+/o, "\n")
@@ -18,6 +28,9 @@ content = []
 content << '<!DOCTYPE html><html><head><title>index</title></head><body><ul>'
 content += YAML.load_file('./http_status_code.yml').reduce([]) { |a, e| a + e['list'] }.map { |data|
   '<li>%d: <a href="%d.html">%s</a></li>' % [data['code'], data['code'], data['text']]
+}
+content += YAML.load_file('./extra.yml').reduce([]) { |a, e| a + e['list'] }.map { |data|
+  '<li><a href="%s.html">%s</a></li>' % [data['file'], data['text']]
 }
 content << '</ul></body></html>'
 File.write 'index.html', content.join("\n") + "\n"
